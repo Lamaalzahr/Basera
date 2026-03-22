2032f9e1d946dbadd937f06bdac824ec7443c1cc
Lamaalzahr <Lamatalzahrani@outlook.com>
2026-03-22 03:42:21 +0300

    تعديل

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -1,11 +1,21 @@
 "use client";
 const Sidebar = () => {
-    return (
-    <div className="space-y-4 py-4 flex-col h-full
-    bg-[#111827] text-white">
-        Hello Sidebar Componet
-
-    </div>
-    );
+return (
+<div className="space-y-4 py-4 flex-col h-full
+bg-[#111827] text-white">
+<div className="px-3 py-2 flex-1">
+<link href="/dashboard" className="flex
+items-center pl-3 mb-14">
+<div className="relative w-8 h-8 mr-4">
+<Image
+fill 
+alt="logo"
+src="/Basera-Logo.png"
+/>
+</div>
+</link>
+</div>
+</div>
+);
 }
 export default Sidebar;
