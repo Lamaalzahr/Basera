ff5fa91afbb799cca5afba1d2d5eeadaab1fe35d
Lamaalzahr <Lamatalzahrani@outlook.com>
2026-04-16 09:09:23 +0300

    fix prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,16 +1,10 @@
 
 import "dotenv/config";
-import { defineConfig } from "prisma/config";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { defineConfig } from "prisma/config"; 
 
 export default defineConfig({
-schema: "prisma/schema.prisma",
-migrations: {
-path: "prisma/migrations",
-},
-adapter: new PrismaPg({
-connectionString: process.env.DATABASE_URL!,
-}),
-}
-
-);
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+});
